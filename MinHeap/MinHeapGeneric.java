cdefede784676b721516c5638a1dddac2bf2702a
anantghuman <105686382+anantghuman@users.noreply.github.com>
2023-05-16 17:51:12 -0500

    Add files via upload

---
+++ MinHeap/MinHeapGeneric.java
@@ -0,0 +1,151 @@
+import java.sql.Array;
+import java.util.Arrays;
+
+public class MinHeapGeneric<T extends Comparable<T>>{
+
+    private T[] heap;
+    private int size;
+    static final int DEFAULT_CAPACITY = 8;
+
+    public MinHeapGeneric(int size){
+        heap = (T[]) new Comparable[size];
+    }
+
+    public MinHeapGeneric(){
+        this(DEFAULT_CAPACITY);
+    }
+
+    public MinHeapGeneric(T... vals){
+        heap = (T[]) new Comparable[vals.length];
+        for(int i = 0; i < vals.length; i++){
+            size++;
+            if(size ==  heap.length)
+                doubleCapacity();
+            heap[i+1] = vals[i];
+        }
+        buildHeap();
+    }
+
+    private void buildHeap(){
+        for(int i = size/2; i > 0; i--)
+            siftDown(i);
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public T peekMinimum(){
+        return heap[1];
+    }
+
+    public int getLeftChild(int index){
+        return index * 2;
+    }
+
+    public int getRightChild(int index){
+        return index * 2 + 1;
+    }
+
+    public int getParentIndex(int index){
+        return index / 2;
+    }
+
+    private void doubleCapacity(){
+        T[] temp = (T[]) new Comparable[heap.length * 2];
+        for(int i = 1; i < size; i++){
+            temp[i] = heap[i];
+        }
+        heap = temp;
+    }
+
+    public void insert(T value){
+        size++;
+        if(size == heap.length)
+            doubleCapacity();
+        heap[size] = value;
+        bubbleUp(size);
+    }
+    private void bubbleUp(int index){
+        int parent = getParentIndex(index);
+        if(parent < 1)
+            return;
+        if(heap[parent].compareTo(heap[index]) > 0) {
+            T temp = heap[parent];
+            heap[parent] = heap[index];
+            heap[index] = temp;
+        }
+        bubbleUp(parent);
+    }
+
+    public T popMinimum(){
+        T temp = heap[1];
+        heap[1] = heap[size];
+        heap[size] = null;
+        size--;
+        siftDown(1);
+        return temp;
+    }
+
+    private void siftDown(int index){
+        int left = getLeftChild(index);
+        int right = getRightChild(index);
+        if(left > size)
+            return;
+        if(right > size) {
+            if (heap[index].compareTo(heap[left]) > 0) {
+                T temp = heap[index];
+                heap[index] = heap[left];
+                heap[left] = temp;
+                siftDown(left);
+            }
+        }
+        else if(heap[right].compareTo(heap[left]) > 0 && heap[index].compareTo(heap[left]) > 0){
+            T temp = heap[index];
+            heap[index] = heap[left];
+            heap[left] = temp;
+            siftDown(left);
+        }
+        else if(heap[left].compareTo(heap[right]) > 0 && heap[right].compareTo(heap[index]) > 0) {
+            T temp = heap[index];
+            heap[index] = heap[right];
+            heap[right] = temp;
+            siftDown(right);
+        }
+    }
+    @Override
+    public String toString()
+    {
+        String output = "";
+        for (int i = 1; i <= getSize(); i++)
+            output += heap[i] + ", ";
+        return output.substring(0, output.lastIndexOf(","));
+    }
+
+    public void display(){
+        int nBlanks = 32, itemsPerRow = 1, column = 0, j = 1;
+        String dots = "...............................";
+        System.out.println(dots + dots);
+        while (j <= this.getSize())
+        {
+            if (column == 0)
+                for (int k = 0; k < nBlanks; k++)
+                    System.out.print(' ');
+            System.out.print((heap[j] == null)? "" : heap[j]);
+            if (++column == itemsPerRow) {
+                nBlanks /= 2;
+                itemsPerRow *= 2;
+                column = 0;
+                System.out.println();
+            } else
+                for (int k = 0; k < nBlanks * 2 - 2; k++)
+                    System.out.print(' ');
+            j++;
+        }
+        System.out.println("\n" + dots + dots);
+    }
+}
---
+++ MinHeap/Runner.java
@@ -0,0 +1,31 @@
+public class Runner
+{
+    public static void main(String[] args)
+    {
+        MinHeapGeneric heap = new MinHeapGeneric();
+
+        int[] nums = {8, 42, 35, 4, -1, 99, 76, 20};
+
+        for (int i = 0; i < nums.length; i++)
+            heap.insert(nums[i]);
+
+        System.out.println("Heap toString: " + heap);
+        System.out.println("\ninitial state of the heap");
+        heap.display();
+
+        heap.insert(36); System.out.println("inserting 36 into the heap...");
+        heap.display();
+
+        heap.insert(3); System.out.println("inserting 3 into the heap...");
+        heap.display();
+
+        System.out.println("Pop min (new min at root) = " + heap.popMinimum());
+        heap.display();
+
+        System.out.println("Peek min (shouldn't change) = " + heap.peekMinimum());
+        heap.display();
+
+        MinHeap buildHeap = new MinHeap(8, 42, 35, 4, -1, 99, 76, 20);
+        buildHeap.display();
+    }
+}
